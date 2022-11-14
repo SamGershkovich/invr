362fccae59f2e690b8519fb3b34f61ea22dea356
Sam Gershkovich <sam@modzee.com>
2022-11-14 17:55:58 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
-Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/products', [ProductController::class, 'search']);
 Route::get('/categories', [CategoryController::class, 'index']);
